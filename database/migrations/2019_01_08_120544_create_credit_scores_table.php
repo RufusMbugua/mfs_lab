7dce1492e1f30a0d876b8051d2aae24d673c2c53
Rufus Ngugi Mbugua <mbuguarufus@gmail.com>
2019-01-08 22:52:22 +0300

    Initial

---
+++ database/migrations/2019_01_08_120544_create_credit_scores_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCreditScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credit_scores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id');
+            $table->double('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credit_scores');
+    }
+}
